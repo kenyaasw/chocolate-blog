b7bc86be31de8daba0e3e8f7bd19bc76116b5321
kenyaasw <37527249+kenyaasw@users.noreply.github.com>
2026-01-02 15:15:08 +0700

    feat : add theme provider and mode toggle
    - add theme provider
    - add dark/light mode toggle

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+
+export default function Navbar() {
+    return (
+        <nav className="w-full relative flex items-center justify-between max-w-7xl mx-auto px-7 py-5">
+            <Link href="/" className="font-bold text-xl">
+                Chocolate<span className="text-primary">Blog</span>
+            </Link>
+
+            <ModeToggle />
+
+        </nav>
+    )
+}
