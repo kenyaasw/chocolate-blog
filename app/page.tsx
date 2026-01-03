96e49e7db8ab88fb42079050893a3d7f4986f4cb
kenyaasw <37527249+kenyaasw@users.noreply.github.com>
2026-01-03 21:14:06 +0700

    fix(sanity): fix UI of the card and show the title and description
    fix UI of the card and show the title and description

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "./components/ModeToggle";
 import Navbar from "./components/Navbar";
 import { client, urlFor } from "./lib/sanity";
 import { simpleBlogCard } from "./lib/interface";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 
 async function getData() {
   const query = `
@@ -26,10 +26,20 @@ export default async function Home() {
   console.log(data);
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5 gap-5">
       {data.map((post, idx) => (
         <Card key={idx}>
-          <Image src={urlFor(post.thumbnailImage).url()} alt={post.title} width={500} height={500} />
+          <Image
+            src={urlFor(post.thumbnailImage).url()}
+            alt={post.title}
+            width={500}
+            height={500}
+            className="rounded-t-lg h-[200px] object-cover"
+          />
+          <CardContent>
+            <CardTitle>{post.title}</CardTitle>
+            <CardDescription>{post.smallDescription}</CardDescription>
+          </CardContent>
         </Card>
       ))}
 
